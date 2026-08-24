9ef593ceba7e6c8b181bfd624203b66861d336f2
lingdensneha ship it <lingdensneha@gmail.com>
2026-08-24 23:13:57 +0545

    successfully added js

---
+++ script1.js
@@ -0,0 +1,28 @@
+/*alert('Script loaded');
+alert('Hello');
+alert('my');
+alert('name');
+alert('is');
+alert('Sneha');
+alert();
+function scriptLoaded(){
+    alert('Script loaded');
+}
+function addition() {
+    var num1 = 60;
+    var num2 = 7;
+    var num3 = num1 + num2;
+    alert(num3);
+}
+addition();/*function call*/
+function displayFullName(){
+    var fullName="Sneha Lingden";
+    alert(fullName);
+}
+displayFullName();
+function showFullName(){
+    var firstName = "Sneha";
+    var lastName = "Lingden";
+    alert(firstName + " " + lastName);
+}
+showFullName();
