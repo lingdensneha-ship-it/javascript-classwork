8c3164d348244048f5f177fb69659bb113eaf92c
lingdensneha ship it <lingdensneha@gmail.com>
2026-08-31 23:01:13 +0545

    successfully added script2

---
+++ script2.js
@@ -0,0 +1,19 @@
+/*function myLoadFunction(){
+    var element=document.getElementById('month');
+    element.firstChild.nodeValue='New month';
+}
+document.addEventListener('DOMContentLoaded',myLoadFunction);
+function myLoadFunction(){
+    var a=document.getElementById('Food');
+    a.firstChild.nodeValue='New Food';
+}
+document.addEventListener('DOMContentLoaded',myLoadFunction);*/
+function myLoadFunction(){
+    var a=document.getElementById('Food');
+    a.firstChild.nodeValue='New Food';
+}
+function hi(){
+document.addEventListener('click',myLoadFunction);
+}
+document.addEventListener('DOMContentLoaded',hi);
+
