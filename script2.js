00c15592e70074e30068a5163413832003a3de84
lingdensneha ship it <lingdensneha@gmail.com>
2026-09-02 21:09:53 +0545

    successfully added script2

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ function myLoadFunction(){
     var a=document.getElementById('Food');
     a.firstChild.nodeValue='New Food';
 }
-document.addEventListener('DOMContentLoaded',myLoadFunction);*/
+document.addEventListener('DOMContentLoaded',myLoadFunction);
 function myLoadFunction(){
     var a=document.getElementById('Food');
     a.firstChild.nodeValue='New Food';
@@ -16,4 +16,19 @@ function hi(){
 document.addEventListener('click',myLoadFunction);
 }
 document.addEventListener('DOMContentLoaded',hi);
+
+
+
+document.addEventListener('DOMContentLoaded',myLoadFunction);*/
+function myLoadFunction(){
+    var element=document.getElementById('myname');
+    alert(element.value)
+}
+
+function hi(){
+    var element=document.getElementById('btn');
+    element.addEventListener('click',myLoadFunction);
+}
+document.addEventListener('DOMContentLoaded',hi);
+
 
